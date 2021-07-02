2f194d31f21dd3e22c3e1b1d0e9102b8fac86c07
Jimmy Saenz <jsaenzr@outlook.com>
2021-07-02 14:15:40 -0600

    Implementar viewBinding para inflar y manejar vista

--- app/src/main/java/me/taggerapp/android/HomeActivity.kt
+++ app/src/main/java/me/taggerapp/android/HomeActivity.kt
@@ -1,11 +1,24 @@
 package me.taggerapp.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.taggerapp.android.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setup()
+    }
+
+    private fun setup() {
+        setupViewBinding()
+    }
+
+    private fun setupViewBinding() {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
