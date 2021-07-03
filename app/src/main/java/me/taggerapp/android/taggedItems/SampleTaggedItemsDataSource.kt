1a5c061a459bf4b2069fddd65735462658202854
Jimmy Saenz <jsaenzr@outlook.com>
2021-07-03 02:45:47 -0600

    Eliminar código innecesario

--- app/src/main/java/me/taggerapp/android/taggedItems/SampleTaggedItemsDataSource.kt
+++ app/src/main/java/me/taggerapp/android/taggedItems/SampleTaggedItemsDataSource.kt
@@ -5,19 +5,6 @@ import kotlin.random.Random
 
 class SampleTaggedItemsDataSource {
 
-    companion object {
-        private const val LIST_LENGTH = 20
-    }
-
-    fun getAll(): List<TaggedItem> {
-        val list = mutableListOf<TaggedItem>()
-        for (counter in 1..LIST_LENGTH) {
-            val newModel = buildModel()
-            list.add(newModel)
-        }
-        return list
-    }
-
     fun buildModel(): TaggedItem {
         val id = UUID.randomUUID().toString()
         return TaggedItem(
