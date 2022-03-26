219b6d62904155bc49e34e33eb9b5191a97fb3cc
Jimmy Saenz <jsaenzr@outlook.com>
2022-03-26 00:06:00 -0600

    Crear DAO para acceder a TaggedItem

---
+++ app/src/main/java/me/taggerapp/android/core/data/database/daos/TaggedItemDao.kt
@@ -0,0 +1,16 @@
+package me.taggerapp.android.core.data.database.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import me.taggerapp.android.core.data.database.entities.TaggedItemEntity
+
+@Dao
+interface TaggedItemDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entity: TaggedItemEntity)
+
+    @Query("SELECT * FROM tagged_items")
+    suspend fun getAll(): List<TaggedItemEntity>
+}
